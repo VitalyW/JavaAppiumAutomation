569bef15a8e32fae8c9a0587519e03f9ceeb5c02
VitalyW <vitzeev1@gmail.com>
2018-11-27 22:38:43 -0800

    Homework 10

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,23 +11,22 @@ import java.net.URL;
 
 public class CoreTestCase extends TestCase {
 
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/vitalykhuzeev/Documents/Code/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        DesiredCapabilities capabilities = getCapabilitiesByPlatformEnv();
+        String platform = System.getenv("PLATFORM");
+        if (platform.equals(PLATFORM_ANDROID)) {
+            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        } else if (platform.equals(PLATFORM_IOS)) {
+            driver = new IOSDriver(new URL(AppiumURL), capabilities);
+        }
         rotateScreenPortrait();
     }
 
@@ -46,6 +46,30 @@ public class CoreTestCase extends TestCase {
 
     protected void backgroundApp(int seconds) {
         driver.runAppInBackground(seconds);
+    }
+
+    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
+        String platform = System.getenv("PLATFORM");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (platform.equals(PLATFORM_ANDROID)) {
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("deviceName", "AndroidTestDevice");
+            capabilities.setCapability("platformVersion", "8.0");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("appPackage", "org.wikipedia");
+            capabilities.setCapability("appActivity", ".main.MainActivity");
+            capabilities.setCapability("app", "/Users/vitalykhuzeev/Documents/Code/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        } else if (platform.equals(PLATFORM_IOS)) {
+            capabilities.setCapability("platformName", "iOS");
+            capabilities.setCapability("deviceName", "iPhone SE");
+            capabilities.setCapability("platformVersion", "11.4");
+            capabilities.setCapability("automationName", "Appium");
+            capabilities.setCapability("app", "/Users/vitalykhuzeev/Documents/Code/JavaAppiumAutomation/apks/Wikipedia.app");
+        } else {
+            throw new Exception("Cannot run the platform from env variable. Platfrom value: " + platform);
+        }
+        return capabilities;
     }
 
 }
--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -1,19 +1,18 @@
 package lib.ui;
 
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.By;
 
 public class SearchPageObject extends MainPageObject {
 
     private static final String
-            SEARCH_INIT_ELEMENT = "//*[contains(@text, 'Search Wikipedia')]",
-            SEARCH_INPUT = "//*[contains(@text, 'Search…')]",
-            SEARCH_CANCEL_BUTTON = "org.wikipedia:id/search_close_btn",
-            SEARCH_RESULT_BY_STRING_TPL= "//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
-            SEARCH_RESULT_ELEMENT = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
-            SEARCH_EMPTY_RESULT_ELEMENT = "org.wikipedia:id/search_empty_text",
-            SEARCH_LIST_ITEM_TITLE = "org.wikipedia:id/page_list_item_title",
-            SEARCH_RESULT_BY_STRING_AND_DESCRIPTION_TPL = "//*[@resource-id='org.wikipedia:id/page_list_item_container'] and contains(., '{SUBSTRING}') and contains(., '{DESCRIPTION}')]";
+            SEARCH_INIT_ELEMENT = "xpath://*[contains(@text, 'Search Wikipedia')]",
+            SEARCH_INPUT = "xpath://*[contains(@text, 'Search…')]",
+            SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn",
+            SEARCH_RESULT_BY_STRING_TPL= "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
+            SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
+            SEARCH_EMPTY_RESULT_ELEMENT = "id:org.wikipedia:id/search_empty_text",
+            SEARCH_LIST_ITEM_TITLE = "id:org.wikipedia:id/page_list_item_title",
+            SEARCH_RESULT_BY_STRING_AND_DESCRIPTION_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container'] and contains(., '{SUBSTRING}') and contains(., '{DESCRIPTION}')]";
 
     public SearchPageObject(AppiumDriver driver) {
         super(driver);
@@ -31,64 +30,64 @@ public class SearchPageObject extends MainPageObject {
 
     public void waitForElementByTitleAndDescription(String title, String description) {
         String xpath = getResultsSearchElementByTitleAndDescription(title, description);
-        this.waitForElementPresent(By.xpath(xpath), "Cannot find search result with title: " + title + " and description: " + description);
+        this.waitForElementPresent(xpath, "Cannot find search result with title: " + title + " and description: " + description);
     }
 
     public void initSearchInput() {
-        this.waitForElementAndClick(By.xpath(SEARCH_INIT_ELEMENT), "Cannot find and click search init element", 10);
-        this.waitForElementPresent(By.xpath(SEARCH_INPUT), "Cannot find search input after clicking search init element");
+        this.waitForElementAndClick(SEARCH_INIT_ELEMENT, "Cannot find and click search init element", 10);
+        this.waitForElementPresent(SEARCH_INPUT, "Cannot find search input after clicking search init element");
     }
 
     public void typeSearchLine(String search_line) {
-        this.waitForElementAndSendKeys(By.xpath(SEARCH_INPUT), search_line,"Cannot find and type into search input", 10);
+        this.waitForElementAndSendKeys(SEARCH_INPUT, search_line,"Cannot find and type into search input", 10);
     }
 
     public void waitForSearchResult(String substring) {
         String search_result_xpath = getResultsSearchElement(substring);
-        this.waitForElementPresent(By.xpath(search_result_xpath), "Cannot find search result with substring " + substring);
+        this.waitForElementPresent(search_result_xpath, "Cannot find search result with substring " + substring);
     }
 
     public void clickByArticleWithSubstring(String substring) {
         String search_result_xpath = getResultsSearchElement(substring);
-        this.waitForElementAndClick(By.xpath(search_result_xpath), "Cannot find and click search result with substring: " + substring, 15);
+        this.waitForElementAndClick(search_result_xpath, "Cannot find and click search result with substring: " + substring, 15);
     }
 
     public void waitForCancelButtonToAppear() {
-        this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON), "Cannot find cancel search button", 10);
+        this.waitForElementPresent(SEARCH_CANCEL_BUTTON, "Cannot find cancel search button", 10);
     }
 
     public void waitForCancelButtonToDisappear() {
-        this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON), "Search cancel button is still present", 10);
+        this.waitForElementNotPresent(SEARCH_CANCEL_BUTTON, "Search cancel button is still present", 10);
     }
 
     public void clickCancelSearch() {
-        this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON), "Cannot find and click cancel search button", 10);
+        this.waitForElementAndClick(SEARCH_CANCEL_BUTTON, "Cannot find and click cancel search button", 10);
     }
 
     public int getAmountOfFoundArticles() {
         this.waitForElementPresent(
-                By.xpath(SEARCH_RESULT_ELEMENT),
+                SEARCH_RESULT_ELEMENT,
                 "Cannot find anything by the given request",
                 15
         );
-        return this.getAmountOfElements(By.xpath(SEARCH_RESULT_ELEMENT));
+        return this.getAmountOfElements(SEARCH_RESULT_ELEMENT);
     }
 
     public void waitForEmptyResultsLabel() {
         this.waitForElementPresent(
-                By.id(SEARCH_EMPTY_RESULT_ELEMENT),
+                SEARCH_EMPTY_RESULT_ELEMENT,
                 "Cannot find empty result label",
                 15
         );
     }
 
     public void assertThereIsNoResultOfSearch() {
-        this.assertElementNotPresent(By.xpath(SEARCH_EMPTY_RESULT_ELEMENT), "We are supposed to not find any results");
+        this.assertElementNotPresent(SEARCH_EMPTY_RESULT_ELEMENT, "We are supposed to not find any results");
     }
 
     public void waitForListItemTitleToAppear() {
         this.waitForElementPresent(
-                By.id(SEARCH_LIST_ITEM_TITLE),
+                SEARCH_LIST_ITEM_TITLE,
                 "Can't find item titles",
                 15
         );
@@ -96,7 +95,7 @@ public class SearchPageObject extends MainPageObject {
 
     public void checkIfItemTitleHasSpecifiedWord(String word, int howManyItemTitlesToCheck) {
         this.ifTitlesHaveSpecificWord(
-                By.id(SEARCH_LIST_ITEM_TITLE),
+                SEARCH_LIST_ITEM_TITLE,
                 word,
                 howManyItemTitlesToCheck
         );
@@ -104,9 +103,9 @@ public class SearchPageObject extends MainPageObject {
 
     public void checkIfAllItemTitlesHaveSpecifiedWord(String word) {
         this.ifTitlesHaveSpecificWord(
-                By.id(SEARCH_LIST_ITEM_TITLE),
+                SEARCH_LIST_ITEM_TITLE,
                 word,
-                this.sizeOfTitles(By.id(SEARCH_LIST_ITEM_TITLE))
+                this.sizeOfTitles(SEARCH_LIST_ITEM_TITLE)
         );
     }
 
